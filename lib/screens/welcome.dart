e1df041cef04d374b477e705367ee4633426a6b7
ipincamp <support@nur-arifin.my.id>
2024-10-26 15:46:46 +0700

    Refactor imports and remove unused components in welcome.dart

--- lib/screens/welcome.dart
+++ lib/screens/welcome.dart
@@ -1,7 +1,5 @@
-import 'package:coffee_shop/components/authButton.dart';
-import 'package:coffee_shop/components/background.dart';
 import 'package:coffee_shop/pallete.dart';
-import 'package:coffee_shop/responsive.dart';
+import 'package:coffee_shop/tipografy.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
@@ -10,90 +8,57 @@ class WelcomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Background(
-      child: SingleChildScrollView(
-        child: SafeArea(
-          child: Responsive(
-            mobile: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Expanded(
-                  child: WelcomeImage(),
-                ),
-                Expanded(
-                  child: Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 20.0),
-                    child: AuthButton(),
+    return Scaffold(
+      backgroundColor: CoffeeShopColors.cream,
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                'Coffee Shop',
+                style: CoffeeShopTextStyles.heading,
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(height: 20),
+              SvgPicture.asset(
+                'assets/icons/coffee.svg',
+                height: 150,
+              ),
+              SizedBox(height: 40),
+              ElevatedButton(
+                onPressed: () {
+                  // Navigator.pushNamed(context, '/register');
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: CoffeeShopColors.coffeeBrown,
+                  padding: EdgeInsets.symmetric(horizontal: 40, vertical: 15),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8),
                   ),
                 ),
-              ],
-            ),
-            desktop: MobileWelcomeScreen(),
+                child: Text(
+                  'Daftar',
+                  style: CoffeeShopTextStyles.button,
+                ),
+              ),
+              SizedBox(height: 10),
+              TextButton(
+                onPressed: () {
+                  // Navigator.pushNamed(context, '/login');
+                },
+                child: Text(
+                  'Masuk',
+                  style: CoffeeShopTextStyles.subheading.copyWith(
+                    color: CoffeeShopColors.coffeeBrown,
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
       ),
-    );
-  }
-}
-
-class MobileWelcomeScreen extends StatelessWidget {
-  const MobileWelcomeScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        WelcomeImage(),
-        Row(
-          children: [
-            Spacer(),
-            Expanded(
-              flex: 8,
-              child: AuthButton(),
-            ),
-            Spacer(),
-          ],
-        ),
-      ],
-    );
-  }
-}
-
-class WelcomeImage extends StatelessWidget {
-  const WelcomeImage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        const Text(
-          "Welcome",
-          style: TextStyle(
-            fontWeight: FontWeight.bold,
-            fontSize: 22,
-          ),
-        ),
-        const SizedBox(
-          height: defaultPadding,
-        ),
-        Row(
-          children: [
-            const Spacer(),
-            Expanded(
-              flex: 8,
-              child: SvgPicture.asset(
-                'assets/icons/coffee.svg',
-                height: 180,
-              ),
-            ),
-            const Spacer(),
-          ],
-        ),
-        const SizedBox(
-          height: defaultPadding * 2,
-        ),
-      ],
     );
   }
 }
