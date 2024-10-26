9684443874393ae0e1799880bc01240d85890a55
ipincamp <support@nur-arifin.my.id>
2024-10-26 14:40:44 +0700

    Refactor color constants in pallete.dart

--- lib/pallete.dart
+++ lib/pallete.dart
@@ -1,6 +1,35 @@
 import 'package:flutter/material.dart';
 
-const colorPrimary = Color(0xFF6F35A5);
-const colorPrimaryLight = Color(0xFFF1E6FF);
+class CoffeeShopColors {
+  /*
+   * Primary Colors
+   *
+   * Background utama aplikasi
+   */
+  static const Color coffeeBrown = Color(0xFF6F4E37);
+  static const Color cream = Color(0xFFD9C7B0);
 
-const double defaultPadding = 16.0;
+  /*
+   * Secondary Colors
+   *
+   * Card background, atau bagian lain yang menonjol namun tetap hangat.
+   */
+  static const Color softBeige = Color(0xFFF3E5C3);
+  static const Color darkChocolate = Color(0xFF3E2723);
+
+  /*
+   * Accent Colors
+   *
+   * Button, icon, atau highlight elemen tertentu untuk memperkaya visual.
+   */
+  static const Color oliveGreen = Color(0xFF8D8F6F);
+  static const Color terracotta = Color(0xFFA0522D);
+
+  /*
+   * Neutral Colors
+   *
+   * Teks, border, atau elemen pendukung.
+   */
+  static const Color ashGrey = Color(0xFFB0A99F);
+  static const Color charcoalBlack = Color(0xFF2D2D2D);
+}
