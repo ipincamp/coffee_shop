31f276b338fd6f41a93bca8809fd85117b88a032
ipincamp <support@nur-arifin.my.id>
2024-10-28 08:39:45 +0700

    Refactor imports and remove unused components in welcome.dart

--- lib/screens/welcome.dart
+++ lib/screens/welcome.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:coffee_shop/pallete.dart';
+import 'package:coffee_shop/screens/login.dart';
 
 class WelcomeScreen extends StatelessWidget {
   const WelcomeScreen({super.key});
@@ -14,6 +16,15 @@ class WelcomeScreen extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            const Text(
+              'Coffee Shop',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+                color: CoffeeShopColors.coffeeBrown,
+              ),
+            ),
+            SizedBox(height: 20),
             SvgPicture.asset(
               'assets/icons/coffee.svg',
               width: imageSize,
@@ -21,17 +32,22 @@ class WelcomeScreen extends StatelessWidget {
             ),
             SizedBox(height: 20),
             SizedBox(
-              width: screenSize.width * 0.30,
+              width: screenSize.width * 0.48,
               child: ElevatedButton(
                 onPressed: () {
-                  // Navigate to login screen
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const LoginScreen(),
+                    ),
+                  );
                 },
                 child: Text('Login'.toUpperCase()),
               ),
             ),
             SizedBox(height: 10),
             SizedBox(
-              width: screenSize.width * 0.30,
+              width: screenSize.width * 0.48,
               child: ElevatedButton(
                 onPressed: () {
                   // Navigate to register screen
@@ -40,6 +56,28 @@ class WelcomeScreen extends StatelessWidget {
               ),
             ),
           ],
+        ),
+      ),
+      bottomNavigationBar: Container(
+        padding: const EdgeInsets.all(10),
+        child: RichText(
+          textAlign: TextAlign.center,
+          text: const TextSpan(
+            text: 'Designed by ',
+            style: TextStyle(
+              color: CoffeeShopColors.ashGrey,
+              fontSize: 10,
+            ),
+            children: <TextSpan>[
+              TextSpan(
+                text: 'ipincamp',
+                style: TextStyle(
+                  color: CoffeeShopColors.darkChocolate,
+                  fontSize: 10,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
